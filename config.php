4d06a6b199b22bda0826eba6842585fc318d6076
Jegor Rysskin <jegorrysskin@gmail.com>
2020-10-27 23:01:59 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
     require('vendor/autoload.php');
     // Define mysql server settings
-    $serverName = "localhost";
-    $username = "root";
-    $password = "";
+    $serverName = "10.0.1.50";
+    $username = "admin";
+    $password = "Urafgt_69";
     $dbName = "employees";
 
     // Create a new mysqli connection. Remember to enable this in php.ini !!
